4445869b451cd1099d9467d83eb07b8291e646fa
tumus2004 <41445493+tumus2004@users.noreply.github.com>
2023-02-11 11:55:39 +1000

    Update upload.tsx

---
+++ components/upload.tsx
@@ -0,0 +1,44 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+export default function FileUpload() {
+  const [file, setFile] = useState<any>(null);
+  const [uploadingStatus, setUploadingStatus] = useState<boolean>(false);
+
+  const uploadFile = async () => {
+    setUploadingStatus(true);
+
+    let { data } = await axios.post('/api/s3/upload', {
+      name: `canvasfun/${file.name}`,
+      type: file.type,
+    });
+
+    const url = data.url;
+    await axios.put(url, file, {
+      headers: {
+        'Content-type': file.type,
+        'Access-Control-Allow-Origin': '*',
+      },
+    });
+
+    setUploadingStatus(false);
+    setFile(null);
+  };
+
+  useEffect(() => {
+    if (file) {
+      const uploadedFileDetail = async () => await uploadFile();
+      uploadedFileDetail();
+    }
+  }, [file]);
+
+  return (
+    <input
+      type='file'
+      accept='image/*'
+      name='image'
+      id='selectFile'
+      onChange={(e: any) => setFile(e.target.files[0])}
+    />
+  );
+}
