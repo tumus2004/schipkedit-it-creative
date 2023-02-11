7b35f77bba261271230d471604ada10101b84a40
tumus2004 <41445493+tumus2004@users.noreply.github.com>
2023-02-11 11:52:01 +1000

    remove env

---
+++ pages/api/s3/upload.ts
@@ -0,0 +1,44 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import S3 from 'aws-sdk/clients/s3';
+
+const s3 = new S3({
+  region: process.env.AWSDEFAULTREGION,
+  accessKeyId: process.env.AWSACCESSKEYID,
+  secretAccessKey: process.env.AWSSECRETACCESSKEY,
+  signatureVersion: 'v4',
+});
+
+const upload = async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method !== 'POST') {
+    return res.status(405).json({ message: 'Method not allowed' });
+  }
+
+  try {
+    let { name, type } = req.body;
+
+    const fileParams = {
+      Bucket: process.env.BUCKET_NAME,
+      Key: name,
+      Expires: 600,
+      ContentType: type,
+      ACL: 'public-read',
+    };
+
+    const url = await s3.getSignedUrlPromise('putObject', fileParams);
+
+    res.status(200).json({ url });
+  } catch (err) {
+    console.log(err);
+    res.status(400).json({ message: err });
+  }
+};
+
+export const config = {
+  api: {
+    bodyParser: {
+      sizeLimit: '40mb', // Set desired value here
+    },
+  },
+};
+
+export default upload;
