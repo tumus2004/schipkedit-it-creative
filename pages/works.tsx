9cfe18af9476912107328fb2f0c60393e0b463a0
tumus2004 <41445493+tumus2004@users.noreply.github.com>
2023-05-20 16:11:06 +1000

    Update works.tsx

--- pages/works.tsx
+++ pages/works.tsx
@@ -1,8 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from '../styles/Works.module.css';
 import listOfRedRisingCharacters from '../components/ListOfRedRisingCharHeights';
 
+type Character = {
+  name: string;
+  faction: string;
+  height: string;
+};
+
 const Works: React.FC = () => {
+  const [sortedCharacters, setSortedCharacters] = useState<Character[]>([]);
+
+  useEffect(() => {
+    const sorted = [...listOfRedRisingCharacters].sort((a, b) => {
+      const aHeight =
+        parseInt(a.height.split("'")[0]) * 12 +
+        parseInt(a.height.split("'")[1]);
+      const bHeight =
+        parseInt(b.height.split("'")[0]) * 12 +
+        parseInt(b.height.split("'")[1]);
+      return bHeight - aHeight;
+    });
+    setSortedCharacters(sorted);
+  }, []);
+
   return (
     <div className={styles.container}>
       <h1 className='text-3xl font-bold text-white mb-4'>
@@ -23,7 +44,7 @@ const Works: React.FC = () => {
             </tr>
           </thead>
           <tbody>
-            {listOfRedRisingCharacters.map((character, index) => (
+            {sortedCharacters.map((character, index) => (
               <tr key={index} className={styles.row}>
                 <td className={`py-2 px-3 text-white ${styles.tableCell}`}>
                   {character.name}{' '}
