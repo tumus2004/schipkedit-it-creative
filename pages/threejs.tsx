722627ae6559d15a0cd74d69c6e8110f53fbdd1a
tumus2004 <41445493+tumus2004@users.noreply.github.com>
2023-06-03 02:16:02 +1000

    wip

---
+++ pages/threejs.tsx
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import * as THREE from "three";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <MyScene />
+      </div>
+    );
+  }
+}
+
+const MyScene = () => {
+  const scene = new THREE.Scene();
+  const camera = new THREE.PerspectiveCamera(
+    75,
+    innerWidth / innerHeight,
+    1,
+    1100
+  );
+  const renderer = new THREE.WebGLRenderer();
+  renderer.setSize(innerWidth, innerHeight);
+  document.getElementById("scene")?.appendChild(renderer.domElement);
+
+  // Create a cube
+  const geometry = new THREE.BoxGeometry(100, 100, 100);
+  const material = new THREE.MeshBasicMaterial({ color: 0x0000ff });
+  const cube = new THREE.Mesh(geometry, material);
+  scene.add(cube);
+
+  // Render the scene
+  renderer.render(scene, camera);
+
+  return <div id="scene"></div>;
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
