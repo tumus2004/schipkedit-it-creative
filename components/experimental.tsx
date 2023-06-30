a45b407d6c2eef32cd34dac9ee073a7475d20df9
tumus2004 <41445493+tumus2004@users.noreply.github.com>
2023-06-30 14:00:31 +1000

    Update experimental.tsx

--- components/experimental.tsx
+++ components/experimental.tsx
@@ -26,32 +26,54 @@ const SolarSystem = () => {
     const textureLoader = new THREE.TextureLoader();
     const texture = textureLoader.load('/texture.jpg');
 
+    // Create the central, larger sphere
+    const centerGeometry = new THREE.SphereGeometry(4, 64, 64);
+    const centerMaterial = new THREE.MeshStandardMaterial({ color: 'yellow' });
+    const centerSphere = new THREE.Mesh(centerGeometry, centerMaterial);
+
+    scene.add(centerSphere);
+
     // Create a sphere geometry with radius 1, and 32 vertical and horizontal segments
     const geometry = new THREE.SphereGeometry(1, 64, 64);
     const material = new THREE.MeshStandardMaterial({ map: texture });
     const sphere = new THREE.Mesh(geometry, material);
 
-    scene.add(sphere);
+    // Position the sphere at the edge of the orbital path
+    sphere.position.x = 15;
+
+    // Create a parent object and add the sphere to it
+    const parentObject = new THREE.Object3D();
+    parentObject.add(sphere);
+
+    scene.add(parentObject);
 
     // Add lighting
     const light = new THREE.PointLight(0xffffff, 1, 100);
-    light.position.set(0, 0, 8);
+    light.position.set(0, 0, 5);
     scene.add(light);
 
-    camera.position.z = 5;
+    camera.position.z = 15;
+    camera.position.y = 2;
 
     // Create a new rotation axis that is tilted 23.5 degrees from the y-axis
     const rotationAxis = new THREE.Vector3(
-      Math.sin(THREE.MathUtils.degToRad(-23.5)),
-      Math.cos(THREE.MathUtils.degToRad(-23.5)),
+      Math.sin(THREE.MathUtils.degToRad(23.5)),
+      Math.cos(THREE.MathUtils.degToRad(23.5)),
       0
     );
+
+    // Adjust the speed of rotation to match Earth's
+    const rotationSpeed = 0.05; 
+    const orbitalSpeed = rotationSpeed / 365; 
 
     const animate = function () {
       requestAnimationFrame(animate);
 
       // Rotate the sphere around the new axis
-      sphere.rotateOnAxis(rotationAxis, 0.01);
+      sphere.rotateOnAxis(rotationAxis, rotationSpeed);
+
+      // Rotate the parent object around the y-axis
+      parentObject.rotation.y += orbitalSpeed;
 
       renderer.render(scene, camera);
     };
@@ -63,6 +85,8 @@ const SolarSystem = () => {
       renderer.dispose();
       material.dispose();
       geometry.dispose();
+      centerMaterial.dispose();
+      centerGeometry.dispose();
     };
   }, [isBrowser]);
 
