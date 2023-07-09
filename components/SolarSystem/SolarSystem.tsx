6271fca4b9b61689235719258561de0d23d2f07a
tumus2004 <41445493+tumus2004@users.noreply.github.com>
2023-07-09 19:59:51 +1000

    Update SolarSystem.tsx

--- components/SolarSystem/SolarSystem.tsx
+++ components/SolarSystem/SolarSystem.tsx
@@ -1,10 +1,6 @@
 import { useEffect, useRef } from 'react';
 import * as THREE from 'three';
 import createPlanet from './helpers/createPlanet';
-import axios from 'axios';
-import dotenv from 'dotenv';
-
-const nasakey = process.env.NEXT_PUBLIC_NASA_API_KEY;
 
 const SolarSystem = () => {
   const containerRef = useRef<HTMLDivElement | null>(null);
@@ -51,7 +47,6 @@ const SolarSystem = () => {
     // Adjust the speed of rotation to match Earth's
     const rotationSpeed = 0.1;
     const orbitalSpeed = rotationSpeed / 365;
-    const marsOrbitalSpeed = rotationSpeed / 687; // Mars orbital speed
 
     // Sun rotation speed
     const sunRotationSpeed = rotationSpeed / 27;
@@ -69,6 +64,14 @@ const SolarSystem = () => {
     );
     scene.add(sun.sphere);
 
+    // Adjust the speed of rotation to match each planet's rotation period
+    const earthRotationSpeed = 0.1; // 1 rotation per day
+    const marsRotationSpeed = earthRotationSpeed * 1.03; // 1.03 rotations per day
+
+    // Adjust the speed of orbit to match each planet's orbital period
+    const earthOrbitalSpeed = earthRotationSpeed / 365.25; // 1 orbit per 365.25 days
+    const marsOrbitalSpeed = earthRotationSpeed / 687; // 1 orbit per 687 days
+
     // Create Earth
     const earth = createPlanet(
       1,
@@ -79,7 +82,7 @@ const SolarSystem = () => {
     );
     const earthPivot = new THREE.Object3D();
     earthPivot.add(earth.sphere);
-    scene.add(earthPivot);
+    sun.sphere.add(earthPivot); // Add the pivot to the sun
 
     // Create the Earth's orbit
     const earthOrbitGeometry = new THREE.RingGeometry(9.99, 10, 64);
@@ -99,48 +102,51 @@ const SolarSystem = () => {
       textureLoader
     );
     const marsPivot = new THREE.Object3D();
+    marsPivot.rotateZ(THREE.MathUtils.degToRad(1.85)); // Tilt the pivot by 1.85 degrees
     marsPivot.add(mars.sphere);
-    scene.add(marsPivot);
+    sun.sphere.add(marsPivot);
 
     // Create the Mars's orbit
-    const marsOrbitGeometry = new THREE.RingGeometry(14.99, 15, 64);
+    const marsOrbitGeometry = new THREE.RingGeometry(14.99, 15.3, 64);
     marsOrbitGeometry.rotateX(Math.PI / 2); // Align the geometry on the xz plane
+    marsOrbitGeometry.rotateZ(THREE.MathUtils.degToRad(1.85)); // Tilt the orbit by 1.85 degrees
     const marsOrbit = new THREE.Line(
       marsOrbitGeometry,
       new THREE.LineBasicMaterial({ color: 0xffffff })
     );
     scene.add(marsOrbit);
 
-    // Fetch current positions of Earth and Mars from NASA HORIZONS API
-    const fetchData = async () => {
-      try {
-        const response = await axios.get(
-          `https://api.nasa.gov/insight_weather/?api_key=${nasakey}&feedtype=json&ver=1.0`
-        );
+    const earthOrbitRadius = 10; // Distance from the Sun
+    const marsOrbitRadius = 15.3; // Distance from the Sun
 
-        const { mars, earth } = response.data;
-        console.log('positions', mars, earth);
+    // Set the positions of Mars and Earth based on current date
+    const setDatePositions = () => {
+      const currentDate = new Date();
 
-        const earthPosition = new THREE.Vector3(
-          earth.position.x,
-          earth.position.y,
-          earth.position.z
-        );
-        earth.sphere.position.copy(earthPosition);
+      // Calculate Earth's position
+      const earthOrbitAngle =
+        (2 * Math.PI * currentDate.getTime()) / (365.25 * 24 * 60 * 60 * 1000); // Earth's orbital angle
+      const earthPosition = new THREE.Vector3(
+        earthOrbitRadius * Math.cos(earthOrbitAngle),
+        0,
+        earthOrbitRadius * Math.sin(earthOrbitAngle)
+      );
+      earthPivot.position.copy(earthPosition);
 
-        const marsPosition = new THREE.Vector3(
-          mars.position.x,
-          mars.position.y,
-          mars.position.z
-        );
-        mars.sphere.position.copy(marsPosition);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
+      // Calculate Mars' position
+      const marsOrbitAngle =
+        (2 * Math.PI * currentDate.getTime()) / (687 * 24 * 60 * 60 * 1000); // Mars' orbital angle
+      const marsPosition = new THREE.Vector3(
+        marsOrbitRadius * Math.cos(marsOrbitAngle),
+        0,
+        marsOrbitRadius * Math.sin(marsOrbitAngle)
+      );
+      marsPivot.position.copy(marsPosition);
     };
 
-    fetchData();
+    setDatePositions();
 
+    // Animation
     // Animation
     const animate = function () {
       requestAnimationFrame(animate);
@@ -150,9 +156,28 @@ const SolarSystem = () => {
       mars.rotate();
       sun.rotate();
 
-      // Rotate the pivot objects around the y-axis
-      earthPivot.rotation.y += orbitalSpeed;
-      marsPivot.rotation.y += marsOrbitalSpeed;
+      // Update the position of the planets along their orbits
+      const currentDate = new Date();
+
+      // Calculate Earth's position
+      const earthOrbitAngle =
+        (2 * Math.PI * currentDate.getTime()) / (365.25 * 24 * 60 * 60 * 1000); // Earth's orbital angle
+      const earthPosition = new THREE.Vector3(
+        earthOrbitRadius * Math.cos(earthOrbitAngle),
+        0,
+        earthOrbitRadius * Math.sin(earthOrbitAngle)
+      );
+      earthPivot.position.copy(earthPosition);
+
+      // Calculate Mars' position
+      const marsOrbitAngle =
+        (2 * Math.PI * currentDate.getTime()) / (687 * 24 * 60 * 60 * 1000); // Mars' orbital angle
+      const marsPosition = new THREE.Vector3(
+        marsOrbitRadius * Math.cos(marsOrbitAngle),
+        0,
+        marsOrbitRadius * Math.sin(marsOrbitAngle)
+      );
+      marsPivot.position.copy(marsPosition);
 
       renderer.render(scene, camera);
     };
@@ -164,7 +189,6 @@ const SolarSystem = () => {
       renderer.dispose();
     };
   }, [isBrowser]);
-
   return <div ref={containerRef} />;
 };
 
