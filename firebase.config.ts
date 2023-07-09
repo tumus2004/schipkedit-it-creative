89747bcb1409118cd40268b42162512c9916e90d
tumus2004 <41445493+tumus2004@users.noreply.github.com>
2023-07-09 13:53:36 +1000

    wip

--- firebase.config.ts
+++ firebase.config.ts
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
 import { getAnalytics } from 'firebase/analytics';
+import dotenv from 'dotenv';
 
 // https://firebase.google.com/docs/web/setup#available-libraries
 
