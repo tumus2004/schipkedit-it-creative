fa1ba11a57ed0f1465bbd1cd302bfccae6846990
tumus2004 <41445493+tumus2004@users.noreply.github.com>
2023-08-10 00:27:43 +1000

    wip

--- components/SolarSystem/utils.ts
+++ components/SolarSystem/utils.ts
@@ -3,11 +3,20 @@ import axios from 'axios';
 export const fetchPlanetData = async () => {
   const BASE_URL = 'https://ssd.jpl.nasa.gov/api/horizons.api';
   const CONFIG_PARAMS =
-    "?format=json&COMMAND='199'&MAKE_EPHEM='YES'&EPHEM_TYPE='VECTORS'&CENTER='@10'&START_TIME='2022-01-01'&STOP_TIME='2022-01-02'&STEP_SIZE='1%20d'";
+    "?format=json&COMMAND='199'&MAKE_EPHEM='YES'&EPHEM_TYPE='VECTORS'&CENTER='@10'&START_TIME='2022-01-01'&STOP_TIME='2023-08-09'&STEP_SIZE='1%20d'";
 
   try {
     const response = await axios.get(`${BASE_URL}${CONFIG_PARAMS}`);
-    console.log(response.data);
+    console.log('response', response);
+    
+    // const positionVelocityStr = response.data.result.split('$$SOE\n')[1].split('$$EOE\n')[0].split('\n')[1];
+    // const [, X, Y, Z, VX, VY, VZ] = positionVelocityStr.split(/\s+/);
+    // const scaleFactor = 1;
+    // const initialX = parseFloat(X) * scaleFactor;
+    // const initialY = parseFloat(Y) * scaleFactor;
+    // const initialZ = parseFloat(Z) * scaleFactor;
+    console.log(response.data.result, typeof response.data.result, 'response.data.result');
+    // console.log(initialX, initialY, initialZ, 'initialX, initialY, initialZ');
   } catch (error) {
     console.log(error);
   }
