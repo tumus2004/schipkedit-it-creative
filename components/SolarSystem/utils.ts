d56f77a0a487e1b6ebc20366c3eef8f1eb4de861
tumus2004 <41445493+tumus2004@users.noreply.github.com>
2023-08-10 22:23:14 +1000

    Update utils.ts

--- components/SolarSystem/utils.ts
+++ components/SolarSystem/utils.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 export const fetchPlanetData = async () => {
   const BASE_URL = 'https://ssd.jpl.nasa.gov/api/horizons.api';
   const CONFIG_PARAMS =
-    "?format=json&COMMAND='199'&MAKE_EPHEM='YES'&EPHEM_TYPE='VECTORS'&CENTER='@10'&START_TIME='2022-01-01'&STOP_TIME='2023-08-09'&STEP_SIZE='1%20d'";
+    "?format=json&COMMAND='199'&MAKE_EPHEM='YES'&EPHEM_TYPE='VECTORS'&CENTER='@10'&START_TIME='2023-08-09'&STOP_TIME='2023-08-09'&STEP_SIZE='1%20d'";
 
   try {
     const response = await axios.get(`${BASE_URL}${CONFIG_PARAMS}`);
