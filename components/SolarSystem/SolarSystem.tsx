46d2d4a8f881c49d783bd93eaec9a3fdf2aa5a2e
tumus2004 <41445493+tumus2004@users.noreply.github.com>
2023-10-03 23:31:14 +1000

    Update SolarSystem.tsx

--- components/SolarSystem/SolarSystem.tsx
+++ components/SolarSystem/SolarSystem.tsx
@@ -661,7 +661,15 @@ const SolarSystem = ({
           />
         ))}
       </div>
-      <div ref={containerRef} style={{ width: '100%', height: '100%' }} />
+      <div
+        ref={containerRef}
+        style={{
+          position: 'absolute',
+          top: '0',
+          width: '100%',
+          height: '100%',
+        }}
+      />
     </div>
   );
 };
