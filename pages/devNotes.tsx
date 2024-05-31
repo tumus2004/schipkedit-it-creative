c7932399cb064b16f0920d75520e49e871e827ea
tumus2004 <41445493+tumus2004@users.noreply.github.com>
2024-05-31 15:33:04 +1000

    Create devNotes.tsx

---
+++ pages/devNotes.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+const DevNotes: React.FC = () => {
+  const notesToRemember = [
+    {
+      id: "1",
+      title: "Shit to remember for devving @ Jumbo",
+      content: "cmd + shift + p, then Restart TS Server (same with ESLint etc)",
+    },
+  ];
+
+  return (
+    <div className="bg-center w-full h-screen bg-no-repeat bg-cover md:bg-fixed">
+      {notesToRemember.map((notes) => (
+        <>
+          <div className="container mx-auto px-6 py-16" key={notes.id}>
+            {notes.id}
+          </div>
+          <div className="container mx-auto px-6 py-16" key={notes.id}>
+            {notes.title}
+          </div>
+          <div className="container mx-auto px-6 py-16" key={notes.id}>
+            {notes.content}
+          </div>
+        </>
+      ))}
+    </div>
+  );
+};
+
+export default DevNotes;
