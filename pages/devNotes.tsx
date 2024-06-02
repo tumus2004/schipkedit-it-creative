6372d88e38c1c4774f9811bb467374986f715497
tumus2004 <41445493+tumus2004@users.noreply.github.com>
2024-06-03 09:54:37 +1000

    Update devNotes.tsx

--- pages/devNotes.tsx
+++ pages/devNotes.tsx
@@ -4,25 +4,38 @@ const DevNotes: React.FC = () => {
   const notesToRemember = [
     {
       id: "1",
-      title: "Shit to remember for devving @ Jumbo",
-      content: "cmd + shift + p, then Restart TS Server (same with ESLint etc)",
+      title: "Restart TypeScript server to solve a bunch of linting issues",
+      content: `cmd + shift + p, then Restart TS Server (same with ESLint etc)`,
+    },
+    {
+      id: "2",
+      title:
+        "Change ownership of node_modules cache folder to avoid permission issues",
+      content: `sudo mkdir -p node_modules/.cache && sudo chmod -R 777 node_modules/.cache`,
     },
   ];
 
   return (
-    <div className="bg-center w-full h-screen bg-no-repeat bg-cover md:bg-fixed">
+    <div id="dev-notes" className="p-4 bg-gray-100 h-screen">
+      <header className="mb-6 text-center">
+        <h1 className="text-4xl font-bold bg-gradient-to-r from-indigo-500 via-sky-500 to-emerald-500 bg-clip-text text-transparent">
+          Dev notes and tips
+        </h1>
+      </header>
+
       {notesToRemember.map((notes) => (
-        <>
-          <div className="container mx-auto px-6" key={notes.id}>
-            {notes.id}
+        <section
+          key={notes.id}
+          className="mb-4 p-6 bg-white rounded-lg shadow-md border-l-4 border-gradient-to-r from-indigo-500 via-sky-500 to-emerald-500"
+        >
+          <div className="ml-4 mb-2 flex items-center gap-2">
+            <div className="text-lg font-bold text-gray-600">{notes.id}:</div>
+            <div className="text-lg text-gray-800">{notes.title}</div>
           </div>
-          <div className="container mx-auto px-6" key={notes.id}>
-            {notes.title}
-          </div>
-          <div className="container mx-auto px-6" key={notes.id}>
-            {notes.content}
-          </div>
-        </>
+          <pre className="p-4 bg-gray-100 rounded-lg overflow-auto">
+            <code className="text-gray-800">{notes.content}</code>
+          </pre>
+        </section>
       ))}
     </div>
   );
