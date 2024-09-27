9c8cd80f8590e4658e501cfb290dbd066d1abc07
tumus2004 <41445493+tumus2004@users.noreply.github.com>
2024-09-27 23:54:23 +1000

    Revert "wip"
    This reverts commit f3a23aa20ce11782112f271108780e01542d33c7.

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,23 +1,13 @@
-import { Html, Head, Main, NextScript } from "next/document";
-import { Analytics } from "@vercel/analytics/react";
+import { Html, Head, Main, NextScript } from 'next/document';
+import { Analytics } from '@vercel/analytics/react';
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang='en'>
       <Head>
         <link
-          href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap"
-          rel="stylesheet"
-        />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="anonymous"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap"
-          rel="stylesheet"
+          href='https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap'
+          rel='stylesheet'
         />
       </Head>
       <body>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,17 @@
 /** @type {import('tailwindcss').Config} */
-
-export const content = [
-  "./pages/**/*.{js,ts,jsx,tsx}",
-  "./components/**/*.{js,ts,jsx,tsx}",
-];
-
-export const theme = {
-  extend: {
-    colors: {
-      "heading-gold": "#a9997a",
-    },
-    fontFamily: {
-      montserrat: ["Montserrat", "sans-serif"],
-    },
-    backgroundImage: {
-      "background-hero": "url('/img/background-hero.png')",
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'heading-gold': '#a9997a',
+      },
+      backgroundImage: {
+        'background-hero': "url('/img/background-hero.png')",
+      },
     },
   },
 };
