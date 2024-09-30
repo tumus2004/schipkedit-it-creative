2e1f0dea58df47f4fb167d9869caa7b6f6fa88e0
tumus2004 <41445493+tumus2004@users.noreply.github.com>
2024-09-30 13:01:46 +1000

    encapsulate panel lighting logic into hook

---
+++ components/LandingHeader/LandingHeader.tsx
@@ -0,0 +1,15 @@
+export const LandingHeader = () => {
+  return (
+    <section className="mb-12 mt-8" id="about">
+      <h1 className="text-4xl pl-2.5 sm:text-5xl md:text-6xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% hover:to-yellow-500 whitespace-nowrap">
+        Hello, I&apos;m Tom.
+      </h1>
+      <p className="mt-6 text-md pl-3.5">
+        I&apos;m a software engineer and web developer with a passion for
+        creating beautiful and responsive websites. With over 5 years of
+        experience in various programming languages and frameworks, I can help
+        bring your ideas to life.
+      </p>
+    </section>
+  );
+};
